471249d5f5ea4defb4170d8ab9ae30d5529a2e89
Ramim Chowdhury <ramim@lamusica.com>
2020-04-04 19:18:49 -0400

    show and change questions as answers pressed

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,42 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var qIndex = 0;
+  var question = [
+    'What\'s your favorite color?',
+    'Where were you born?',
+    'What is your hobby?',
+    "What is your name?"
+  ];
+
+  void answeredQuestion(){
+    print("chosen Answer");
+    setState(() {
+      qIndex++;
+      if(qIndex==3){qIndex=0;}
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Scaffold(
       appBar: AppBar(title: Text("My App")),
-      body: Text("A Body text"),
+      body: Column(
+        children: <Widget>[
+          Text(question[qIndex]),
+          RaisedButton(child: Text("Answer 1"), onPressed: answeredQuestion,),
+          RaisedButton(child: Text("Answer 2"), onPressed: answeredQuestion,),
+          RaisedButton(child: Text("Answer 3"), onPressed: answeredQuestion,),
+        ],
+      ),
     ),
     );
   }
