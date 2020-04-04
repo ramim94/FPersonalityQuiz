4546721ed45cb85650e05a1ef977c4f5e0328979
Ramim Chowdhury <ramim@lamusica.com>
2020-04-04 19:44:33 -0400

    added styling to question widget

--- lib/question.dart
+++ lib/question.dart
@@ -9,6 +9,15 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(_questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(2.0),
+      child: Text(_questionText,
+        style: TextStyle(
+          fontSize: 20,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    ) ;
   }
 }
